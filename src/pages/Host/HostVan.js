b1f3575f035270f603f98457a02e0d21d63d06cf
codingWithFaheem <faheemk5914858@gmail.com>
2023-06-15 19:38:26 +0500

    added

--- src/pages/Host/HostVan.js
+++ src/pages/Host/HostVan.js
@@ -1,7 +1,39 @@
 import React from 'react'
+import { useVanContext } from '../../Context/AppContextProvider'
+import { Link } from 'react-router-dom'
 
-export const HostVan = () => {
+export const HostVan = () => { 
+    const {vanData} = useVanContext()
+    const hostVansEls = vanData.map(van => (
+        <Link
+            to={`/host/vans/${van.id}`}
+            key={van.id}
+            className="host-van-link-wrapper"
+        >
+            <div className="host-van-single" key={van.id}>
+                <img src={van.imgUrl} alt={`Photo of ${van.name}`} />
+                <div className="host-van-info">
+                    <h3>{van.name}</h3>
+                    <p>${van.price}/day</p>
+                </div>
+            </div>
+        </Link>
+    ))
   return (
-    <div>HostVan</div>
+    <section>
+            <h1 className="host-vans-title">Your listed vans</h1>
+            <div className="host-vans-list">
+                {
+                    vanData.length > 0 ? (
+                        <section>
+                            {hostVansEls}
+                        </section>
+
+                    ) : (
+                            <h2>Loading...</h2>
+                        )
+                }
+            </div>
+        </section>
   )
 }
--- src/pages/Host/HostVanDetails.js
+++ src/pages/Host/HostVanDetails.js
@@ -1,7 +1,50 @@
-import React from 'react'
 
+import React, { useEffect, useState } from 'react'
+import { doc, getDoc } from 'firebase/firestore/lite'
+import { useParams } from 'react-router-dom'
+import { db } from '../../config/firebase'
 export const HostVanDetails = () => {
+    const [van ,setVan] = useState(null)
+    const params = useParams()
+    const getVan = async() => {
+        const docRef =   doc(db,"van",params.id)
+    
+      try{
+            const singleVanData = await getDoc(docRef)
+             setVan({
+                ...singleVanData.data(),
+                id : singleVanData.id 
+            })
+           
+    
+      }
+      catch(Err){
+        console.error(Err)
+      }
+    
+    }
+    useEffect(() => {
+        getVan()
+    }, []);
+    if(!van){
+        return <h1>Loading...</h1>
+    }
   return (
-    <div>HostVanDetails</div>
+    <section>
+    <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+            <img src={van.imgUrl} />
+            <div className="host-van-detail-info-text">
+                <i
+                    className={`van-type van-type-${van.type}`}
+                >
+                    {van.type}
+                </i>
+                <h3>{van.name}</h3>
+                <h4>${van.price}/day</h4>
+            </div>
+        </div>
+    </div>
+</section>
   )
 }
