e956e8a57be374a693b808424a5048021eecf40f
codingWithFaheem <faheemk5914858@gmail.com>
2023-06-15 22:33:22 +0500

    added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom" ;
 import { useVanContext } from '../Context/AppContextProvider';
 
 export const Home = () => {
-  const {van} = useVanContext()
-  console.log(van)
+
   return (
-<div className="home-container">
+<div className="border home-container">
+  <div className=' home-sec'>
             <h1>You got the travel plans, we got the travel vans.</h1>
             <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
             <Link to="vans">Find your van</Link>
+    </div>
+    <img src="./images/van.png" alt="van"  className='home-img'/>
+
         </div>
   )
 }
